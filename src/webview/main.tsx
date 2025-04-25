d66bd021d09189979b703302890d4dbf956b71c5
CRH-ayyamada <141080366+yamada1023@users.noreply.github.com>
2025-04-25 14:57:52 +0900

    update: 見た目作成

--- src/webview/main.tsx
+++ src/webview/main.tsx
@@ -1,14 +1,48 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import { VSCodeButton, VSCodeTextField } from "@vscode/webview-ui-toolkit/react";
 
 const main = () => {
-  const [text, setText] = useState<string>("");
+  const [hour, setHour] = useState("");
+  const [minutes, setMinutes] = useState("");
+  const [minError, setMinError] = useState("");
+  const [hourError, setHourError] = useState("");
+
+  const checkHourValidate = (e: any) => {
+    const newValue = e.target.value;
+    if (/^\d*$/.test(newValue)) {
+      setHour(newValue);
+      setHourError("");
+    } else {
+      setHourError("hour: 数字のみを入力してください");
+    }
+  };
+
+  const checkMinValidate = (e: any) => {
+    const newValue = e.target.value;
+    if (/^\d*$/.test(newValue)) {
+      setMinutes(newValue);
+      setMinError("");
+    } else {
+      setMinError("minutes: 数字のみを入力してください");
+    }
+  };
 
   return (
     <>
-      <VSCodeButton onClick={(e) => setText("ボタンがクリックされました")}>ボタン</VSCodeButton>
-      <div>{text}</div>
+      <p>タイマーを設定</p>
+      <div className="content-wrap">
+        <VSCodeTextField placeholder="hour" className="vscode-text-field" value={hour} oninput={checkHourValidate} />
+        <span className="textfield-span">:</span>
+        <VSCodeTextField placeholder="minutes" className="vscode-text-field" value={minutes} oninput={checkMinValidate} />
+      </div>
+      {hourError && (
+        <span className="error">{hourError}</span>
+      )}
+      {minError && (
+        <span className="error">{minError}</span>
+      )}
+      <VSCodeButton className="vscode-button">START</VSCodeButton>
     </>
   );
 };
