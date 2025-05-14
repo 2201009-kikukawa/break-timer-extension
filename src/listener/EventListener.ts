caa08ccb4b4fa6fdb37c8202a09ec8c1006e6285
CRH-ayyamada <141080366+yamada1023@users.noreply.github.com>
2025-05-14 11:12:27 +0900

    update: タイマー処理追加

--- src/listener/EventListener.ts
+++ src/listener/EventListener.ts
@@ -1,5 +1,85 @@
 import { WebviewView, window } from "vscode";
+import * as vscode from "vscode";
 import { EventListenerProps, EventTypes } from "../types/classNames";
+
+let statusBarItem: vscode.StatusBarItem;  // ステータスバー管理
+let countdownTimer: NodeJS.Timeout | undefined; // タイマー管理
+let remainingSeconds = 0; // 残り秒数
+let isRunning = false;  // true: タイマー動作中, false: タイマー停止中
+
+// タイマー開始
+function startCountdown(seconds: number) {
+  clearInterval(countdownTimer);
+  remainingSeconds = seconds;
+  isRunning = true;
+
+  updateStatusBar(remainingSeconds);
+
+  // カウントダウン
+  countdownTimer = setInterval(() => {
+    remainingSeconds--;
+    updateStatusBar(remainingSeconds);
+
+    // タイマーが0になったらモーダル表示
+    if (remainingSeconds <= 0) {
+      clearInterval(countdownTimer);
+      const selection = window.showInformationMessage(
+        'おつかれさまでした！休憩中にストレッチをしませんか？',
+        { modal: true },
+        "ストレッチをする"
+      );
+
+      // ステータスバー非表示
+      clearStatusBar();
+      isRunning = false;
+    }
+  }, 1000);
+}
+
+// 一時停止
+function pauseCountDown() {
+  if (countdownTimer) {
+    clearInterval(countdownTimer);
+    isRunning = false;
+  }
+}
+
+// 終了
+function stopCountDown() {
+  if (countdownTimer) {
+    clearInterval(countdownTimer);
+  }
+  remainingSeconds = 0;
+  clearStatusBar();
+  isRunning = false;
+
+  // 終了した旨を通知
+  const selection = window.showInformationMessage('タイマーを停止しました');
+}
+
+// ステータスバー更新
+function updateStatusBar(seconds: number) {
+  // ステータスバーが存在しなければ作成
+  if (!statusBarItem) {
+    statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+  }
+
+  // ステータスバー表示
+  statusBarItem.show();
+
+  // 残り時間表示
+  const hours = Math.floor(seconds / 60);
+  const minutes = seconds % 60;
+  statusBarItem.text = `$(clock) ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+}
+
+// ステータスバー非表示
+function clearStatusBar() {
+  if (statusBarItem) {
+    statusBarItem.hide();
+  }
+}
+
 
 export class EventListener {
   public setWebviewMessageListener(webviewView: WebviewView) {
@@ -7,12 +87,28 @@ export class EventListener {
       const type = message.type;
       const text = message.text;
 
-      if (type === EventTypes.showModal) {
-        const selection = window.showInformationMessage (
-          text,
-          { modal: true },
-          "OK"
-        );
+      if (type === EventTypes.init) {
+        // 初期状態をReactに送信
+        webviewView.webview.postMessage({
+          type: EventTypes.init,
+          running: isRunning,
+          time: remainingSeconds
+        });
+      } else if (type === EventTypes.startTimer) {
+        startCountdown(parseInt(text));
+      } else if (type === EventTypes.pauseTimer) {
+        pauseCountDown();
+
+        // 現在の値をReactに送信
+        webviewView.webview.postMessage({
+          type: EventTypes.pauseTimer,
+          running: isRunning,
+          time: remainingSeconds
+        });
+      } else if (type === EventTypes.restartTimer) {
+        startCountdown(parseInt(text));
+      } else if (type === EventTypes.clearTimer) {
+        stopCountDown();
       }
     });
   }
--- src/webview/main.tsx
+++ src/webview/main.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { VSCodeButton, VSCodeTextField } from "@vscode/webview-ui-toolkit/react";
 import { EventListenerProps, EventTypes } from "../types/classNames";
+import { VSCodeButton, VSCodeTextField } from "@vscode/webview-ui-toolkit/react";
 
 // VSCode API使用
 declare const acquireVsCodeApi: () => {
@@ -10,14 +10,43 @@ declare const acquireVsCodeApi: () => {
 const vscode = acquireVsCodeApi();
 
 const main = () => {
-  const [hour, setHour] = useState("");
-  const [minutes, setMinutes] = useState("");
+  // 初期状態
+  const [isRunning, setIsRunning] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+
+  // 入力
+  const [hours, setHours] = useState('');
+  const [minutes, setMinutes] = useState('');
+
+  // エラー
   const [hourError, setHourError] = useState("");
   const [minError, setMinError] = useState("");
 
+  useEffect(() => {
+    vscode.postMessage({
+      type: EventTypes.init,
+      text: ''
+    });
+
+    const handleMessage = (event: MessageEvent) => {
+      const { type, running, time } = event.data;
+
+      if (type === EventTypes.init) { // 状態チェック
+        setIsRunning(running);
+        setSeconds(time);
+      } else if (type === EventTypes.pauseTimer) {  // 残り時間格納
+        setSeconds(time);
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
+  }, []);
+
+  // 入力値チェック
   const checkHourValidate = (e: any) => {
     const newValue = e.target.value;
-    setHour(newValue);
+    setHours(newValue);
 
     if (!/^\d*$/.test(newValue)) { return setHourError("時間：数字のみを入力してください"); }
 
@@ -34,33 +63,89 @@ const main = () => {
   };
 
   const checkValidate = () => {
-    if (!(hourError || minError) && (hour && minutes)) {
-      // タイマー開始処理
-      vscode.postMessage({
-        type: EventTypes.showModal,
-        text: "メッセージ"
-      });
-      return;
+    if (seconds > 0) {
+      handleRestart();
+    } else if (!(hourError || minError) && (hours && minutes)) {
+      handleStart();
     }
-    hourError ? setHour("") : "";
-    minError ? setMinutes("") : "";
+
+    if (hourError) { setHours(''); }
+    if (minError) { setMinutes(''); }
+  };
+
+  // スタート
+  const handleStart = () => {
+    const h = parseInt(hours, 10) || 0;
+    const m = parseInt(minutes, 10) || 0;
+    const secs = h * 3600 + m * 60;
+
+    vscode.postMessage({
+      type: EventTypes.startTimer,
+      text: secs.toString()
+    });
+
+    setIsRunning(true);
+  };
+
+  // 一時停止
+  const handlePause = () => {
+    vscode.postMessage({
+      type: EventTypes.pauseTimer,
+      text: ''
+    });
+
+    setIsRunning(false);
+  };
+
+  // リスタート
+  const handleRestart = () => {
+    vscode.postMessage({
+      type: EventTypes.restartTimer,
+      text: seconds.toString()
+    });
+
+    setIsRunning(true);
+  };
+
+  // 停止
+  const handleStop = () => {
+    vscode.postMessage({
+      type: EventTypes.clearTimer,
+      text: ''
+    });
+
+    setHours('');
+    setMinutes('');
+    setIsRunning(false);
   };
 
   return (
     <>
-      <p>タイマーを設定</p>
-      <div className="content-wrap">
-        <VSCodeTextField placeholder="hour" className="vscode-text-field" value={hour} oninput={checkHourValidate} />
-        <span className="textfield-span">:</span>
-        <VSCodeTextField placeholder="minutes" className="vscode-text-field" value={minutes} oninput={checkMinValidate} />
-      </div>
-      {hourError && (
-        <span className="error">{hourError}</span>
-      )}
-      {minError && (
-        <span className="error">{minError}</span>
-      )}
-      <VSCodeButton className="vscode-button" onClick={checkValidate}>START</VSCodeButton>
+      <div>
+        <h3>作業時間を設定</h3>
+
+        <div className="content-wrap">
+          <VSCodeTextField placeholder="時間" className="vscode-text-field" value={hours} oninput={checkHourValidate} />
+          <span className="textfield-span">:</span>
+          <VSCodeTextField placeholder="分" className="vscode-text-field" value={minutes} oninput={checkMinValidate} />
+        </div>
+
+        {hourError && (
+          <p className="error">{hourError}</p>
+        )}
+        {minError && (
+          <p className="error">{minError}</p>
+        )}
+
+        <div className="vscode-button-wrap">
+          {isRunning ?
+            <VSCodeButton className="vscode-button" onclick={handlePause}>一時停止</VSCodeButton> :
+            <VSCodeButton className="vscode-button" onclick={checkValidate}>スタート</VSCodeButton>
+          }
+
+          <VSCodeButton className="vscode-button" appearance="secondary" onClick={handleStop}>終了</VSCodeButton>
+        </div>
+      </div >
     </>
   );
 };
